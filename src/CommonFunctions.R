63e74d2038b3f7f47bef68aae33ea3eeb8c9d53e
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2021-05-22 13:47:55 +0200

    init

---
+++ src/CommonFunctions.R
@@ -0,0 +1,15 @@
+install_packages <- function() {
+  packages <- c(
+    "ggplot2",
+    "rpart",
+    "rpart.plot",
+    "gmodels",
+    "e1071",
+    "gridExtra",
+    "randomForest"
+  )
+  
+  install.packages(packages)
+  
+  
+}
