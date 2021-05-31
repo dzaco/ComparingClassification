5c0790358d3b67e7a9f0b8583a568f465f53ae52
kocend11 <kocend11@gmail.com>
2021-05-31 19:51:50 +0200

    Add iris description in report

--- src/Main.R
+++ src/Main.R
@@ -1,7 +1,16 @@
 source("src/CommonFunctions.R")
 install_packages()
 
+##################################################################
+#Iris
+##################################################################
 data(iris)
+summary(iris)
+
+cr <- cor(iris[1:4])
+corrplot(cr, method = "circle")
+
+iris = clean(iris)
 summary(iris)
 
 train = create_train_dataset(iris, 100, 123)
@@ -22,6 +31,8 @@ CrossTable(test$Species, preds.rpart, chisq = F, prop.r = F, prop.c = F, prop.t 
 time <- Sys.time() - time.start
 times <- c(times, time)
 algorithms <- c(algorithms, "decision_trees")
+decision_trees_accuracy = ((16+20+12)/nrow(test))*100
+decision_trees_accuracy
 
 #k-Nearest Neighbours
 time.start <- Sys.time()
@@ -31,6 +42,8 @@ time.end <- Sys.time()
 time <- Sys.time() - time.start
 times <- c(times, time)
 algorithms <- c(algorithms, "k_nearest_neighbours")
+nearest_neighbours_accuracy = ((16+19+13)/nrow(test))*100
+nearest_neighbours_accuracy
 
 #Support Vector Machine
 time.start <- Sys.time()
@@ -39,6 +52,8 @@ CrossTable(preds.svm,test$Species, chisq = F, prop.r = F, prop.c = F, prop.t = F
 time <- Sys.time() - time.start
 times <- c(times, time)
 algorithms <- c(algorithms, "support_vector_machine")
+support_vector_machine_accuracy = ((16+20+13)/nrow(test))*100
+support_vector_machine_accuracy
 
 #Random Forest
 time.start <- Sys.time()
@@ -47,6 +62,9 @@ CrossTable(preds.rf, test$Species, chisq = F, prop.r = F, prop.c = F, prop.t = F
 time <- Sys.time() - time.start
 times <- c(times, time)
 algorithms <- c(algorithms, "random_forest")
+random_forest_accuracy = ((16+19+13)/nrow(test))*100
+random_forest_accuracy
+
 
 time.table$algorithm <- algorithms
 time.table$time <- times
@@ -58,3 +76,17 @@ which(preds.rpart != preds.rf)
 which(preds.knn != preds.svm)
 which(preds.knn != preds.rf)
 which(preds.svm != preds.rf)
+
+
+##################################################################
+#Imports-85
+##################################################################
+
+data("imports85")
+summary(imports85)
+
+imports85 = clean(imports85)
+summary(imports85)
+
+cr <- cor(imports85[1:26])
+corrplot(cr, method = "circle")
